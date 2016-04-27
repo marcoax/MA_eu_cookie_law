4057f1481ea3bcebde510f73efd2d1015dd7f18e
marco asperti <marcoasperti@gmail.com>
2016-04-27 20:33:19 +0200

    #css -update

--- widgets_cookie_eu.inc.php
+++ widgets_cookie_eu.inc.php
@@ -1,9 +1,8 @@
 <?php 
 //DEFINE SOME CONFIG VARIABLES
 $cookie_name    = 'ma_eu'; //cookie name
-$privacy_url    = '/privacy.php'; //  privacy page url
 $lang    		= ( isset($_GET['lang']) && $_GET['lang']!='' ) ? $_GET['lang']: 'en'; // set the  language
-
+$privacy_url    = '/privacy.php?lang='.$lang; //  privacy page url
 $cookie_message = array(
 		'en' => array(
 				   'string' =>'This site uses cookies. By continuing to browse the site you are agreeing to our use of cookies',
